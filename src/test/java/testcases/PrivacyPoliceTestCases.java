8942ff22e385dfc83b37cbaaf11dee3f8f60a89c
Apurbokabbo <apurbokabbo1999@gmail.com>
2023-06-11 18:18:35 +0600

    Privacy police page testcase update

--- src/test/java/testcases/PrivacyPoliceTestCases.java
+++ src/test/java/testcases/PrivacyPoliceTestCases.java
@@ -98,7 +98,100 @@ public class PrivacyPoliceTestCases extends BaseDriver {
 		
 	}
 	
+	@Test
+	public void logFilesBodyVerify() throws InterruptedException{
+		getDriver().get(privacyPoliceObj.PrivacyPolice_URL);
+		
+		privacyPoliceObj.assertionMethod(privacyPoliceObj.LOG_FILES_BUTTON_LOCATOR, privacyPoliceObj.LOG_FILE_BODY_LOCATOR, privacyPoliceObj.LOG_FILE_BODY_TEXT);
+		
+		
+	}
 	
+	@Test
+	public void advertisingPartnerTittleVerify() throws InterruptedException{
+		getDriver().get(privacyPoliceObj.PrivacyPolice_URL);
+		
+		privacyPoliceObj.assertionMethod(privacyPoliceObj.ADVERTISING_PARTNER_BUTTON_LOCATOR, privacyPoliceObj.ADVERTISING_TITTLE_LOCATOR, privacyPoliceObj.ADVERTISING_TITTLE_TEXT);
+		
+		
+	}
+	
+//	@Test
+//	public void advertisingPartnerBodyVerify() throws InterruptedException{
+//		getDriver().get(privacyPoliceObj.PrivacyPolice_URL);
+//		
+//		privacyPoliceObj.assertionMethod(privacyPoliceObj.ADVERTISING_PARTNER_BUTTON_LOCATOR, privacyPoliceObj.ADVERTISING_BODY_LOCATOR, privacyPoliceObj.ADVERTISING_BODY_TEXT);
+//		
+//		
+//	}
+	
+	@Test
+	public void thirthPartyTittleVerify() throws InterruptedException{
+		getDriver().get(privacyPoliceObj.PrivacyPolice_URL);
+		
+		privacyPoliceObj.assertionMethod(privacyPoliceObj.THIRTH_PARTY_BUTTON_LOCATOR, privacyPoliceObj.THIRTH_PARTY_TITTLE_LOCATOR, privacyPoliceObj.THIRTH_PARTY_TITTLE_TEXT);
+		
+		
+	}
+	
+//	@Test
+//	public void thirthPartyBodyVerify() throws InterruptedException{
+//		getDriver().get(privacyPoliceObj.PrivacyPolice_URL);
+//		
+//		privacyPoliceObj.assertionMethod(privacyPoliceObj.THIRTH_PARTY_BUTTON_LOCATOR, privacyPoliceObj.THIRTH_PARTY_BODY_LOCATOR, privacyPoliceObj.THIRTH_PARTY_BODY_TEXT);
+//		
+//		
+//	}
+	
+	@Test
+	public void ccpaPrivacyTittleVerify() throws InterruptedException{
+		getDriver().get(privacyPoliceObj.PrivacyPolice_URL);
+		
+		privacyPoliceObj.assertionMethod(privacyPoliceObj.CCPA_BUTTON_LOCATOR, privacyPoliceObj.CCPA_TITTLE_LOCATOR, privacyPoliceObj.CCPA_TITTLE_TEXT);
+		
+		
+	}
+	
+//	@Test
+//	public void ccpaPrivacyBodyVerify() throws InterruptedException{
+//		getDriver().get(privacyPoliceObj.PrivacyPolice_URL);
+//		
+//		privacyPoliceObj.assertionMethod(privacyPoliceObj.CCPA_BUTTON_LOCATOR, privacyPoliceObj.CCPA_BODY_LOCATOR, privacyPoliceObj.CCPA_BODY_TEXT);
+//		
+//		
+//	}
+	
+	
+	@Test
+	public void GPDRTittleVerify() throws InterruptedException{
+		getDriver().get(privacyPoliceObj.PrivacyPolice_URL);
+		
+		privacyPoliceObj.assertionMethod(privacyPoliceObj.GPDR_BUTTON_LOCATOR, privacyPoliceObj.GPDR_TITTLE_LOCATOR, privacyPoliceObj.GPDR_TITTLE_TEXT);
+		
+		
+	}
+	
+	
+//	@Test
+//	public void GPDRBodyVerify() throws InterruptedException{
+//		getDriver().get(privacyPoliceObj.PrivacyPolice_URL);
+//		
+//		privacyPoliceObj.assertionMethod(privacyPoliceObj.GPDR_BUTTON_LOCATOR, privacyPoliceObj.GPDR_BODY_LOCATOR, privacyPoliceObj.GPDR_BODY_TEXT);
+//		
+//		
+//	}
+	
+	@Test
+	public void topButtonVerify() throws InterruptedException {
+		getDriver().get(privacyPoliceObj.PrivacyPolice_URL);
+		privacyPoliceObj.findElement(privacyPoliceObj.GPDR_BUTTON_LOCATOR).click();
+		Thread.sleep(1000);
+		privacyPoliceObj.assertionHard(privacyPoliceObj.GPDR_TITTLE_LOCATOR, privacyPoliceObj.GPDR_TITTLE_TEXT);
+		privacyPoliceObj.findElement(privacyPoliceObj.TOP_BUTTON_LOCATOR).click();
+		Thread.sleep(1000);
+		privacyPoliceObj.assertionHard(privacyPoliceObj.CONSENT_TITTLE_LOCATOR, privacyPoliceObj.CONSENT_TITTLE_TEXT);
+		
+	}
 	
 	
 
